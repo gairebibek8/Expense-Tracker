6385844a41331b91b32351f0968e3be29b52c435
tmoore1210 <tmmoore1210@gmail.com>
2022-11-22 23:27:05 -0600

    Accounts component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Title } from './components/Title';
 import { Balance } from './components/Balance';
+import { Accounts } from './components/Accounts';
 import './App.css';
 
 
@@ -10,6 +11,7 @@ function App() {
       <Title/>
         <div className="container">
           <Balance/>
+          <Accounts/>
         </div>
     </div>
   );
---
+++ src/components/Accounts.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Accounts = () => {
+  return (
+    <>
+    <div>
+        <h3>Income</h3>
+        <p id="add-money" class="add money">+$0.00</p>
+    </div>
+    <div>
+        <h3>Expenses</h3>
+        <p id="sub-money" class="sub money">+$0.00</p>
+    </div>
+    </>
+  )
+}
