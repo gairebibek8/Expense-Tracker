fce5d69d3dcbfd001f83f8f3e0182e310ccafb42
tmoore1210 <tmmoore1210@gmail.com>
2022-11-23 03:17:00 -0600

    Transactions component added

---
+++ src/components/Transactions.js
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from 'react'
+import {GloabalContext} from '../context/GlobalState';
+
+export const Actions = () => {
+  const[description, setDescription] = useState('')
+  const[quantity, setQuantity] = useState(0)
+
+  const {addActions} = useContext(GloabalContext)
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newActions = {
+        id: Math.floor(Math.random() * 100000000),
+        description,
+        quantity: +quantity
+    }
+    addActions(newActions);
+  }
+
+    return (
+    <>
+        <div>
+            <h3>Enter a new transcation.</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-control">
+                    <label htmlFor="description">Description</label>
+                    <input type="text" value ={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter your transaction's description."/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="quantity">Quantity 
+                    <br>A negative quantity will be accounted for an expense.</br>
+                    <br>A positive quantity will be accounted for as income.</br>
+                    </label>
+                    <input type="number" value ={quantity} onChange={(e) => setQuantity(e.target.value)} placeholder="Enter your transaction's quantity."/>
+                </div>
+                <button className="btn">Add transaction to your history</button>
+            </form>
+        </div>
+    </>
+  )
+}
