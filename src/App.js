2e41e2e285f378ed4dc20fac480407e1d234b246
tmoore1210 <tmmoore1210@gmail.com>
2022-11-22 23:00:48 -0600

    Title component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from 'react';
+import { Title } from './components/Title';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Title/>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Title = () => {
+  return (
+    <div>
+       <h2>Your Expense Tracker</h2> 
+    </div>
+  )
+}
