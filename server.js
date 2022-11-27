5f4eff03e93b307e4a0c13f1c1f0dcd32937f709
Bibek Gaire <gairebibek8@gmail.com>
2022-11-26 19:31:43 -0600

    Mongodb setup completed

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: "./config/config.env" });
 
-const app = express();
-const PORT = process.env.PORT || 2000;
+connectDB();
 
-app.get('/', (req, res) => res.send('Hello!'));
+const transactions = require("./routes/transactions");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// app.get('/', (req, res) => res.send('Hello!'));
+app.use("/api/v1/transactions", transactions);
 
 app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}!`.green.bold));
