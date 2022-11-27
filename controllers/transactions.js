309d77b7be88690e6558dc7197d34414672b3ee8
Bibek Gaire <gairebibek8@gmail.com>
2022-11-26 21:40:52 -0600

    Actual database name updated for CRUD functions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,17 +1,56 @@
+const Transactions = require("../model/Transactions");
+
+
+
 // @desc     Get all transactions
 // @route    GET  /api/v1/transactions
 // @access   Public
-exports.getTransactions = (req, res, next) => {
-    res.send("GET transactions");
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transactions = await Transactions.find();
 
-}
+        return res.status(200).json({
+            success: true,
+            count: transactions.length,
+            data: transactions
+          });
+        } catch (err) {
+          return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+          });
+        }
+      }
 
 
 // @desc     Add transactions
 // @route    POST  /api/v1/transactions
 // @access   Public
-exports.addTransaction = (req, res, next) => {
-    res.send("POST transactions");
+exports.addTransaction = async (req, res, next) => {
+    try {
+        const { text, amount } = req.body;
+    
+        const transactions = await Transactions.create(req.body);
+      
+        return res.status(201).json({
+          success: true,
+          data: transactions
+        }); 
+      } catch (err) {
+        if(err.name === 'ValidationError') {
+          const messages = Object.values(err.errors).map(val => val.message);
+    
+          return res.status(400).json({
+            success: false,
+            error: messages
+          });
+        } else {
+          return res.status(500).json({
+            success: false,
+            error: 'Server Error'
+          });
+        }
+      }
 
 }
 
@@ -19,7 +58,29 @@ exports.addTransaction = (req, res, next) => {
 // @desc     Delete transaction
 // @route    DELETE  /api/v1/transactions/:id
 // @access   Public
-exports.deleteTransaction = (req, res, next) => {
-    res.send("DELETE transaction");
+exports.deleteTransaction = async (req, res, next) => {
+    try {
+        const transactions = await Transactions.findById(req.params.id);
+    
+        if(!transactions) {
+          return res.status(404).json({
+            success: false,
+            error: 'No transaction found'
+          });
+        }
+    
+        await transactions.remove();
+    
+        return res.status(200).json({
+          success: true,
+          data: {}
+        });
+    
+      } catch (err) {
+        return res.status(500).json({
+          success: false,
+          error: 'Server Error'
+        });
+      }
 
 }
--- server.js
+++ server.js
@@ -11,6 +11,9 @@ connectDB();
 const transactions = require("./routes/transactions");
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 // app.get('/', (req, res) => res.send('Hello!'));
